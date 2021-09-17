e58c194de28021246887edf828a7db45cf3b662e
bronifty <bronifty@gmail.com>
2021-09-17 17:16:03 -0400

    .

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -5,10 +5,11 @@ import Link from 'next/link';
 
 const Navbar = () => (
   <nav>
-    <Link href="/products">Products</Link>
-    <Link href="/sell">Sell</Link>
-    <Link href="/orders">Orders</Link>
-    <Link href="/account">Account</Link>
+    <Link href='/products'>Products</Link>
+    <Link href='/sell'>Sell</Link>
+    <Link href='/orders'>Orders</Link>
+    <Link href='/account'>Account</Link>
+    <Link href='/admin'>Admin</Link>
   </nav>
 );
 export default Navbar;
